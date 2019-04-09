fcd45cd12d79267fd1a226bfc86f395df33d1c97
sts2606 <41682520+sts2606@users.noreply.github.com>
2019-04-09 11:57:53 +0300

    social hover done, callback done

--- src/js/script/callback.js
+++ src/js/script/callback.js
@@ -1,4 +1,16 @@
 (function () {
+    $(document).ready(function() {
+        let width = $("body").css("width");
+            let widthValue = parseInt(width);
+            if(widthValue < 481) {
+                $(".callback__title").text("Subscribe To Stay  In Touch");
+                $(".callback__info-field--button").text("Subscribe");
+            } else {
+                $(".callback__title").text("Order callbak");
+                $(".callback__info-field--button").text("call me!");
+            }
+    })
+    
     $(window).resize(function() {
             let width = $("body").css("width");
             let widthValue = parseInt(width);
