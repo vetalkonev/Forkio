6f7c23f12eea81fa10384801e241f7469fab4cea
sts2606 <41682520+sts2606@users.noreply.github.com>
2019-04-10 19:49:05 +0300

    edit carousel

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,9 @@
 })();
 (function () {
 	$('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        infinite: true,
         prevArrow: '<img src="./src/img/carousel/ArrowL.png" alt="icon" class="prewArrow">',
         nextArrow: '<img src="./src/img/carousel/ArrowR.png" alt="icon" class="nextArrow">'
     });
